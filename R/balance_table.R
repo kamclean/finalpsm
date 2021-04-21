ff2ba4e0a2d7ad035ca3b49932565440f9e08172
kamclean <mcleankaca@gmail.com>
2021-04-21 15:22:20 +0100

    update balance table to handle levels that are not matched

--- R/balance_table.R
+++ R/balance_table.R
@@ -105,112 +105,122 @@ balance_table <- function(matchit_out, threshold = 0.2, p=FALSE){
 
   if(length(var_factor)>=1){
 
-  # Determine the weighted
+    # Determine the weighted
 
-  sum_fac <- data %>%
-    dplyr::select_at(vars(c("strata_01", all_of(var_factor), "weights"))) %>%
-    dplyr::group_split(strata_01) %>%
-    purrr::map_df(function(x){x %>%
-        dplyr::group_by(strata_01) %>%
-        dplyr::summarise(across(c(all_of(var_factor), weights), function(x){list(x)}), .groups = "drop") %>%
-        tidyr::pivot_longer(cols= -all_of(c("weights", "strata_01")), names_to = "label", values_to = "data") %>%
-        dplyr::select(strata_01, label, data, weights) %>%
-        tidyr::unnest(cols = everything()) %>%
-        dplyr::group_by(strata_01, label, data) %>%
-        dplyr::summarise(.groups = "drop",
-                         n_unm = n(),
-                         n_mat = sum(weights, na.rm = T) %>% round())})
+    sum_fac <- data %>%
+      dplyr::select_at(vars(c("strata_01", all_of(var_factor), "weights"))) %>%
+      dplyr::group_split(strata_01) %>%
+      purrr::map_df(function(x){x %>%
+          dplyr::group_by(strata_01) %>%
+          dplyr::summarise(across(c(all_of(var_factor), weights), function(x){list(x)}), .groups = "drop") %>%
+          tidyr::pivot_longer(cols= -all_of(c("weights", "strata_01")), names_to = "label", values_to = "data") %>%
+          dplyr::select(strata_01, label, data, weights) %>%
+          tidyr::unnest(cols = everything()) %>%
+          dplyr::group_by(strata_01, label, data) %>%
+          dplyr::summarise(.groups = "drop",
+                           n_unm = n(),
+                           n_mat = sum(weights, na.rm = T) %>% round())})
 
-  data_fac <- sum_fac %>%
-    dplyr::group_by(strata_01, label,data ) %>%
-    dplyr::mutate(unm = map(n_unm, function(x){rep(data, x)}),
-                  mat = map(n_mat, function(x){rep(data, x)})) %>%
-    dplyr::ungroup() %>%
-    dplyr::select(-data, -n_mat, -n_unm) %>%
-    tidyr::pivot_longer(cols = c("unm", "mat"), names_to = "match", values_to = "data") %>%
-    tidyr::unnest(cols = "data") %>%
-    dplyr::mutate(label_level = paste0(label, "___", data))
+    data_fac <- sum_fac %>%
+      dplyr::group_by(strata_01, label,data ) %>%
+      dplyr::mutate(unm = map(n_unm, function(x){rep(data, x)}),
+                    mat = map(n_mat, function(x){rep(data, x)})) %>%
+      dplyr::ungroup() %>%
+      dplyr::select(-data, -n_mat, -n_unm) %>%
+      tidyr::pivot_longer(cols = c("unm", "mat"), names_to = "match", values_to = "data") %>%
+      tidyr::unnest(cols = "data") %>%
+      dplyr::mutate(label_level = paste0(label, "___", data))
 
-  smd_fac_unmatch <- data_fac %>%
-    dplyr::filter(match == "unm") %>%
-    dplyr::group_split(label) %>%
-    purrr::map_df(function(x){stddiff::stddiff.category(data = as.data.frame(x),
-                                                        gcol = "strata_01",
-                                                        vcol = "label_level") %>%
-        as.data.frame() %>%
-        tibble::rownames_to_column(var = "label_level")}) %>%
-    dplyr::mutate(label_level = stringr::str_sub(label_level, 4, nchar(label_level))) %>%
-    tidyr::separate(col = "label_level", into = c("label", "level"), sep = "___") %>%
-    dplyr::select(label:level,"prop_unm_con" = p.c,"prop_unm_trt" = p.t, "unm_smd" = stddiff)  %>%
-    dplyr::group_by(label) %>%
-    tidyr::fill(unm_smd, .direction = "downup") %>%
-    dplyr::ungroup() %>%
-    dplyr::mutate(label = as.character(label),
-                  level = as.character(level))
-
-
-  smd_fac_match <- data_fac %>%
-    dplyr::filter(match == "mat") %>%
-    dplyr::group_split(label) %>%
-    purrr::map_df(function(x){stddiff::stddiff.category(data = as.data.frame(x),
-                                                     gcol = "strata_01",
-                                                     vcol = "label_level") %>%
-        as.data.frame() %>%
-        tibble::rownames_to_column(var = "label_level")}) %>%
-    dplyr::mutate(label_level = stringr::str_sub(label_level, 4, nchar(label_level))) %>%
-    tidyr::separate(col = "label_level", into = c("label", "level"), sep = "___") %>%
-    dplyr::select(label:level,"prop_mat_con" = p.c,"prop_mat_trt" = p.t, "mat_smd" = stddiff) %>%
-    dplyr::group_by(label) %>%
-    tidyr::fill(mat_smd, .direction = "downup") %>%
-    dplyr::ungroup() %>%
-    dplyr::mutate(label = as.character(label),
-                  level = as.character(level))
-
-  test_fac <- sum_fac %>%
-    dplyr::select(-data) %>%
-    tidyr::pivot_longer(cols = c("n_unm", "n_mat"), names_to = "match", values_to = "n") %>%
-    tidyr::pivot_wider(names_from = c("strata_01"), values_from = "n", values_fn = list) %>%
-    tidyr::unnest(cols = c(`0`, `1`)) %>%
-    dplyr::group_split(label, match) %>%
-    purrr::map_df(function(x){
-
-      info <- x %>% dplyr::select(match, label) %>% dplyr::distinct()
-
-      p <- x %>% dplyr::select(`0`, `1`) %>% chisq.test() %>% broom::tidy() %>% select("p" = p.value)
-
-      return(out = dplyr::bind_cols(info, p))}) %>%
-    dplyr::mutate(match = stringr::str_remove(match, "n_")) %>%
-    tidyr::pivot_wider(names_from = c("match"), values_from = "p") %>%
-    dplyr::select(label, "unm_p" = unm,"mat_p" = mat)
+    smd_fac_unmatch <- data_fac %>%
+      dplyr::filter(match == "unm") %>%
+      dplyr::group_split(label) %>%
+      purrr::map_df(function(x){stddiff::stddiff.category(data = as.data.frame(x),
+                                                          gcol = "strata_01",
+                                                          vcol = "label_level") %>%
+          as.data.frame() %>%
+          tibble::rownames_to_column(var = "label_level")}) %>%
+      dplyr::mutate(label_level = stringr::str_sub(label_level, 4, nchar(label_level))) %>%
+      tidyr::separate(col = "label_level", into = c("label", "level"), sep = "___") %>%
+      dplyr::select(label:level,"prop_unm_con" = p.c,"prop_unm_trt" = p.t, "unm_smd" = stddiff)  %>%
+      dplyr::group_by(label) %>%
+      tidyr::fill(unm_smd, .direction = "downup") %>%
+      dplyr::ungroup() %>%
+      dplyr::mutate(label = as.character(label),
+                    level = as.character(level))
 
 
-  tab_bal_fac <- sum_fac %>%
-    dplyr::rename("level" = data) %>%
-    dplyr::mutate(strata_01 = ifelse(strata_01==0, "con", "trt")) %>%
-    tidyr::pivot_wider(names_from = "strata_01",
-                       values_from = c("n_unm", "n_mat")) %>%
-    dplyr::mutate(level = as.character(level)) %>%
-    dplyr::left_join(smd_fac_unmatch, by = c("label", "level")) %>%
-    dplyr::left_join(smd_fac_match, by = c("label", "level")) %>%
-    dplyr::left_join(test_fac, by = c("label")) %>%
-    dplyr::mutate(unm_trt = paste0(n_unm_trt, " (",
-                                       scales::percent(prop_unm_trt, accuracy = 0.1,  suffix = ""), ")"),
-                  unm_con = paste0(n_unm_con, " (",
-                                       scales::percent(prop_unm_con, accuracy = 0.1,  suffix = ""), ")"),
-                  mat_trt = paste0(n_mat_trt, " (",
-                                       scales::percent(prop_mat_trt, accuracy = 0.1,  suffix = ""), ")"),
-                  mat_con = paste0(n_mat_con, " (",
-                                       scales::percent(prop_mat_con, accuracy = 0.1, suffix = ""), ")")) %>%
-    dplyr::select(label, level,
-                  unm_con, unm_trt,unm_smd,unm_p,
-                  mat_con, mat_trt,mat_smd,mat_p)}
+    smd_fac_match <- data_fac %>%
+      dplyr::filter(match == "mat") %>%
+      dplyr::group_split(label) %>%
+      purrr::map_df(function(x){stddiff::stddiff.category(data = as.data.frame(x),
+                                                          gcol = "strata_01",
+                                                          vcol = "label_level") %>%
+          as.data.frame() %>%
+          tibble::rownames_to_column(var = "label_level")}) %>%
+      dplyr::mutate(label_level = stringr::str_sub(label_level, 4, nchar(label_level))) %>%
+      tidyr::separate(col = "label_level", into = c("label", "level"), sep = "___") %>%
+      dplyr::select(label:level,"prop_mat_con" = p.c,"prop_mat_trt" = p.t, "mat_smd" = stddiff) %>%
+      dplyr::group_by(label) %>%
+      tidyr::fill(mat_smd, .direction = "downup") %>%
+      dplyr::ungroup() %>%
+      dplyr::mutate(label = as.character(label),
+                    level = as.character(level))
+
+    test_fac <- sum_fac %>%
+      tidyr::pivot_longer(cols = c("n_unm", "n_mat"), names_to = "match", values_to = "n") %>%
+      tidyr::pivot_wider(names_from = c("strata_01"), values_from = "n") %>%
+      dplyr::mutate(across(c(`0`, `1`), function(x){ifelse(is.na(x)==T, 0, x)})) %>%
+      dplyr::select(-data) %>%
+      dplyr::group_split(label, match) %>%
+
+      purrr::map_df(function(x){
+
+        info <- x %>% dplyr::select(match, label) %>% dplyr::distinct()
+
+        p <- tryCatch(x %>% dplyr::select(`0`, `1`) %>% chisq.test(),
+                      warning=function(w){return(x %>% dplyr::select(`0`, `1`) %>% fisher.test(workspace=2e+07,hybrid=TRUE))})  %>%
+          broom::tidy() %>%
+          dplyr::select("p" = p.value)
+
+
+        return(out = dplyr::bind_cols(info, p))}) %>%
+      dplyr::mutate(match = stringr::str_remove(match, "n_")) %>%
+      tidyr::pivot_wider(names_from = c("match"), values_from = "p") %>%
+      dplyr::select(label, "unm_p" = unm,"mat_p" = mat)
+
+
+    tab_bal_fac <- sum_fac %>%
+      dplyr::rename("level" = data) %>%
+      dplyr::mutate(strata_01 = ifelse(strata_01==0, "con", "trt")) %>%
+      tidyr::pivot_wider(names_from = "strata_01",
+                         values_from = c("n_unm", "n_mat")) %>%
+      dplyr::mutate(level = as.character(level)) %>%
+      dplyr::left_join(smd_fac_unmatch, by = c("label", "level")) %>%
+      dplyr::left_join(smd_fac_match, by = c("label", "level")) %>%
+      dplyr::left_join(test_fac, by = c("label")) %>%
+      dplyr::mutate(unm_trt = paste0(n_unm_trt, " (",
+                                     scales::percent(prop_unm_trt, accuracy = 0.1,  suffix = ""), ")"),
+                    unm_con = paste0(n_unm_con, " (",
+                                     scales::percent(prop_unm_con, accuracy = 0.1,  suffix = ""), ")"),
+                    mat_trt = paste0(n_mat_trt, " (",
+                                     scales::percent(prop_mat_trt, accuracy = 0.1,  suffix = ""), ")"),
+                    mat_con = paste0(n_mat_con, " (",
+                                     scales::percent(prop_mat_con, accuracy = 0.1, suffix = ""), ")")) %>%
+      dplyr::select(label, level,
+                    unm_con, unm_trt,unm_smd,unm_p,
+                    mat_con, mat_trt,mat_smd,mat_p)}
 
   # Create final table-------------------
   tab_bal_final <- dplyr::bind_rows(tab_bal_fac, tab_bal_num) %>%
     dplyr::mutate(label = factor(label, levels=dependent),
                   mat_p = ifelse(mat_p<0.001, "<0.001", format(round(mat_p, 3), nsmall=3)),
                   unm_p = ifelse(unm_p<0.001, "<0.001", format(round(unm_p, 3), nsmall=3))) %>%
-    dplyr::arrange(label)
+    dplyr::arrange(label) %>%
+    dplyr::mutate(across(c("unm_con", "unm_trt", "mat_con", "mat_trt"),
+                         function(x){stringr::str_replace_all(x, "NA \\(0.0\\)|0 \\(NA\\)|NA \\(NA\\)", "0 (0.0)")})) %>%
+    dplyr::group_by(label) %>%
+    tidyr::fill(mat_smd, .direction = "downup") %>%
+    dplyr::ungroup()
 
   if(is.null(threshold)==F){
     if(is.numeric(threshold)==T){
